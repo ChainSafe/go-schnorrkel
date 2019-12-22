6333b5ca2d191cdf9c9519af9b9c1ee3cb3116aa
noot <elizabethjbinks@gmail.com>
2019-12-21 23:41:03 -0500

    add invalid test case

--- vrf_test.go
+++ vrf_test.go
@@ -9,6 +9,7 @@ import (
 func TestVRFSignAndVerify(t *testing.T) {
 	signTranscript := merlin.NewTranscript("vrf-test")
 	verifyTranscript := merlin.NewTranscript("vrf-test")
+	verify2Transcript := merlin.NewTranscript("vrf-test")
 
 	priv, pub, err := GenerateKeypair()
 	if err != nil {
@@ -27,5 +28,19 @@ func TestVRFSignAndVerify(t *testing.T) {
 
 	if !ok {
 		t.Fatal("did not verify vrf")
+	}
+
+	proof.c, err = NewRandomScalar()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ok, err = pub.VrfVerify(verify2Transcript, inout, proof)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if ok {
+		t.Fatal("verified invalid proof")
 	}
 }
