0193f739520c1206cee256cea645230a0d0fc76c
noot <elizabethjbinks@gmail.com>
2021-02-19 18:41:47 -0500

    add should not verify test

--- batch_test.go
+++ batch_test.go
@@ -30,3 +30,27 @@ func TestBatchVerify(t *testing.T) {
 	require.NoError(t, err)
 	require.True(t, ok)
 }
+
+func TestBatchVerify_Bad(t *testing.T) {
+	num := 16
+	transcripts := make([]*merlin.Transcript, num)
+	sigs := make([]*Signature, num)
+	pubkeys := make([]*PublicKey, num)
+
+	for i := 0; i < num; i++ {
+		transcript := merlin.NewTranscript(fmt.Sprintf("hello_%d", i))
+		priv, pub, err := GenerateKeypair()
+		require.NoError(t, err)
+
+		sigs[i], err = priv.Sign(transcript)
+		require.NoError(t, err)
+
+		transcripts[i] = merlin.NewTranscript(fmt.Sprintf("hello_%d", i))
+		pubkeys[i] = pub
+	}
+
+	transcripts[6] = merlin.NewTranscript(fmt.Sprintf("hello_%d", 999))
+	ok, err := VerifyBatch(transcripts, sigs, pubkeys)
+	require.NoError(t, err)
+	require.False(t, ok)
+}
